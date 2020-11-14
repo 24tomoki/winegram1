f3327567e6e5a6f11932ed7b846a69701a1c79d0
EC2 Default User <ec2-user@ip-172-31-21-150.ap-northeast-1.compute.internal>
2020-11-14 06:05:22 +0000

    edit update

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -16,6 +16,12 @@ class WinesController < ApplicationController
     @wine = Wine.find(params[:id])
   end
   
+  def destroy
+    wine = Wine.find(params[:id])  # データ（レコード）を1件取得
+    wine.destroy  # データ（レコード）を削除
+    redirect_to wines_path
+  end
+  
   
   def create
     # データ新規登録するインスタンス
@@ -23,16 +29,21 @@ class WinesController < ApplicationController
    wine.user_id = current_user.id
    #wine.image_id = wine_params[:image]
     # データを保存する
-   wine.save!
+   if wine.save
     # トップ画面にリダイレクトする
-   redirect_to wines_path
+    redirect_to wines_path
+   else 
+     @wine = Wine.new
+     render 'new'
+   end
+   
   end
   
   
   def update
-    @wine = Wine.find(params[:id])
-    @wine.update(wine_params)
-    redirect_to wines_path
+    wine = Wine.find(params[:id])
+    wine.update(wine_params)
+    redirect_to user_path(current_user)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_for :admins, controllers: {
-  sessions: 'admins/sessions'}
+    sessions: 'admins/sessions'
+  }
   resources :data_pages
   resources :users
   
